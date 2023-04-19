2928756195b9fc91e3acbd9f026820e9c773394c
LAPTOP-SOURRGL5\anjel <anjelika.kocheva@abv.bg>
2023-04-19 12:44:15 +0300

    feat: reseting password if you entered it wrong, also reseting it after 5sec if correct

--- src/SecretPassowrd.ts
+++ src/SecretPassowrd.ts
@@ -1,12 +1,28 @@
-export class SecretPassowrd {
+export module SecretPassowrd {
 
-	public fullCombination: any[][] = [[],[],[]];
+	export class CreatePass {
+		public fullCombination: any[][] = [[], [], []];
 
-	constructor(max: number, min: number){
-		for(let i = 0 ; i < 3 ; ++i){
-			let direction: string = Math.random() > 0.5 ? "clockwise" : "counterclockwise";
-			let times = Math.floor(Math.random() * (max - min + 1)) + min;
-			this.fullCombination[i].push(times, direction);
+		constructor(max: number, min: number) {
+			for (let i = 0; i < 3; ++i) {
+				let direction: string = Math.random() > 0.5 ? "clockwise" : "counterclockwise";
+				let times = Math.floor(Math.random() * (max - min + 1)) + min;
+				this.fullCombination[i].push(times, direction);
+			}
+		}
+	}
+
+	export class GetPass {
+		public password = new SecretPassowrd.CreatePass(9, 1).fullCombination;
+		// make a copy (get value without reference)
+		public copyOfPassword: any[][] = JSON.parse(JSON.stringify(this.password));
+
+		show() {
+			console.log("Shhs the secret password is:");
+
+			this.copyOfPassword.forEach(element => {
+				console.log(element.join(' '));
+			});
 		}
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 import { SecretPassowrd } from './SecretPassowrd';
 import { TimerApp } from './Timer';
 
-
 let count = new TimerApp.Timer;
 let time = setInterval(function(){
 	count.countUp();
   }, 1000);
 
-
 // Get secret password
-let password = new SecretPassowrd(9, 1).fullCombination;
+let get = new SecretPassowrd.GetPass;
+get.show();
 
-// make a copy (get value without reference)
-let copyOfPassword: any[][] = JSON.parse(JSON.stringify(password));
-
-console.log("Shhs the secret password is:");
-
-copyOfPassword.forEach(element => {
-	console.log(element.join(' '));
-});
+function reset(){
+	count.second = 0;
+	console.clear();
+	get = new SecretPassowrd.GetPass;
+	get.show();
+}
 
 const buttons = document.querySelectorAll(".button");
 
@@ -28,31 +25,36 @@ buttons.forEach(button => {
 		let rotate: string = "0";
 
 		// if clicked right direction
-		if (copyOfPassword[0][1].match("counterclockwise") && button.getAttribute("id") == "counterclockwise") {
+		if (get.copyOfPassword[0][1].match("counterclockwise") && button.getAttribute("id") == "counterclockwise") {
 			rotate = "-60";
-		} else if (!copyOfPassword[0][1].match("counterclockwise") && button.getAttribute("id") == "clockwise") {
+		} else if (!get.copyOfPassword[0][1].match("counterclockwise") && button.getAttribute("id") == "clockwise") {
 			rotate = "+60";
 		}
 		else {
 			console.log("start again");
 			rotate = "0";
-			copyOfPassword = JSON.parse(JSON.stringify(password));
-			count.second = 0;
+			reset();
 		}
 
 		// if rotation was correct -1
 		if (rotate != '0') {
-			copyOfPassword[0][0] -= 1
+			get.copyOfPassword[0][0] -= 1
 		};
 
 		// if times == 0 , removes the completed array
-		if (copyOfPassword[0][0] == 0) {
-			copyOfPassword.splice(0, 1);
+		if (get.copyOfPassword[0][0] == 0) {
+			get.copyOfPassword.splice(0, 1);
 		}
 
-		if (copyOfPassword.length == 0) {
+		if (get.copyOfPassword.length == 0) {
 			console.log("Yey you win");
 			clearInterval(time);
+			setTimeout(function() {
+				reset();
+				setInterval(function(){
+					count.countUp();
+				  }, 1000)
+			}, 5000);
 		}
 
 	});
